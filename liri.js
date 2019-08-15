e8afc6a4eb0faac901db7e3ca1a7481ffe812a60
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-08-15 18:00:46 -0500

    created an if statement for options

--- liri.js
+++ liri.js
@@ -21,18 +21,20 @@ var fs = require("fs");
 var command = process.argv[2];
 // console.log("This is what was typed after liri.js " + command);
 
+var options = "";
+if (process.argv[3]) {
 // need to grab 4th text on the command line and loop through it to make it a usable if result is "All the Small Things"
-var options = process.argv[3].split(" ").join("+");
-
+options = process.argv[3].split(" ").join("+");
+};
 // runThings(command, options);
 
 // function runThings(command, options) {
-
-//     options = processOptions();
-
+    
+    //     options = processOptions();
+    
     //switch is the if/else for all the commands to liri
     switch (command) {
-
+        
         case "spotify-this-song":
 
             var songString = options;
@@ -52,7 +54,28 @@ var options = process.argv[3].split(" ").join("+");
 
         case "do-what-it-says":
 
+            // var whatTheyChoose = options;
+
+            // console.log(whatItSay());
             
+            fs.readFile("random.txt", "utf8", function (error, data) {
+
+                // If the code experiences any errors it will log the error to the console.
+                if (error) {
+                    return console.log(error);
+                }
+
+                // We will then print the contents of data
+                console.log(data);
+
+                // Then split it by commas (to make it more readable)
+                var dataArr = data.split(",");
+
+                // We will then re-display the content as an array for later use.
+                console.log(dataArr);
+
+            });
+
             break;
 
         case "concert-this":
@@ -122,22 +145,22 @@ spotify.lookup({ type: 'track', query: '3DYVWvPh3kGwPasp7yjahc' }, function (err
 });
 };
 
-function whatItSay() {
-    fs.readFile("random.txt", "utf8", function (error, data) {
+// function whatItSay() {
+//     fs.readFile("random.txt", "utf8", function (error, data) {
 
-                // If the code experiences any errors it will log the error to the console.
-                if (error) {
-                    return console.log(error);
-                }
+//                 // If the code experiences any errors it will log the error to the console.
+//                 if (error) {
+//                     return console.log(error);
+//                 }
 
-                // We will then print the contents of data
-                // console.log(data);
+//                 // We will then print the contents of data
+//                 console.log(data);
 
-                // Then split it by commas (to make it more readable)
-                var dataArr = data.split(",");
+//                 // Then split it by commas (to make it more readable)
+//                 var dataArr = data.split(",");
 
-                // We will then re-display the content as an array for later use.
-                console.log(dataArr);
+//                 // We will then re-display the content as an array for later use.
+//                 console.log(dataArr);
 
-            });
-}
+//             });
+// }
