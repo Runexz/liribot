ae33ff59bc865aac7a7476e6a98c2111ae28502e
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-08-16 18:37:32 -0500

    dowhatitsays works correctly

--- liri.js
+++ liri.js
@@ -100,10 +100,11 @@ switch (command) {
             console.log(data);
 
             // Then split it by commas (to make it more readable)
-            var dataArr = data.split(",");
+            // var dataArr = data.split(",");
 
-            // We will then re-display the content as an array for later use.
-            console.log(dataArr);
+            // // We will then re-display the content as an array for later use.
+            // console.log(dataArr);
+            songinfo(data);
 
         });
 
